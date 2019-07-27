b0553c76e420aafd0101331027b835243fe25e2d
Ricardo Gaya <rrgaya@gmail.com>
2019-07-27 06:36:08 -0300

    Corringido modelos e add admin dos modelos

--- source/healtcheck/admin.py
+++ source/healtcheck/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Endereco, Verificacao
 
-# Register your models here.
+
+@admin.register(Endereco)
+class EnderecoAdmin(admin.ModelAdmin):
+    list_display = ('url', 'ativo')
+
+
+@admin.register(Verificacao)
+class VerificacaoAdmin(admin.ModelAdmin):
+    list_display = ('endereco', 'status')
+
+
+# admin.site.register(Endereco, EnderecoAdmin)
+# admin.site.register(Verificacao, VerificacaoAdmin)
--- source/healtcheck/models.py
+++ source/healtcheck/models.py
@@ -3,8 +3,11 @@ from django.db import models
 
 
 class Endereco(models.Model):
-    url = models.BooleanField()
+    url = models.URLField()
     ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.url
 
 
 class Verificacao(models.Model):
@@ -18,4 +21,7 @@ class Verificacao(models.Model):
     )
 
     endereco = models.ForeignKey(Endereco, on_delete=models.SET_NULL, null=True, blank=True)
-    status = models.BooleanField(choices=CHOICES_STATUS)
+    status = models.IntegerField(choices=CHOICES_STATUS)
+
+    def __str__(self):
+        return self.endereco
